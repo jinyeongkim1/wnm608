7b6496e8d0b325ee3f6a053847f3b5cbe42dc8de
jinyeongkim1 <107874179+jinyeongkim1@users.noreply.github.com>
2022-07-20 18:34:24 -0500

    Add files via upload

---
+++ kim.jinyeong/about.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>About Page</title>
+
+	<?php include "parts/meta.php"; ?>
+</head>
+<body>
+
+	<?php include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">
+			<h2>About Us</h2>
+			<p>We're pretty cool!</p>
+		</div>
+	</div>
+
+</body>
+</html>
---
+++ kim.jinyeong/parts/meta.php
@@ -0,0 +1,7 @@
+<meta name="viewport" content="width=device-width">
+
+<base href="http://jinyeongkim.com/aau/wnm608/kim.jinyeong/">
+
+<link rel="stylesheet" href="lib/css/styleguide.css">
+<link rel="stylesheet" href="lib/css/gridsystem.css">
+<link rel="stylesheet" href="css/storetheme.css">

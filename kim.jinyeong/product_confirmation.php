9e5d947980305c3200860c32b4e21ebe747187f9
jinyeongkim1 <107874179+jinyeongkim1@users.noreply.github.com>
2022-07-24 01:03:20 -0500

    Add files via upload

---
+++ kim.jinyeong/product_confirmation.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Confirmation Page</title>
+
+	<?php include "parts/meta.php"; ?>
+</head>
+<body>
+
+	<?php include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">
+			<h2>Thank you for your purchases</h2>
+			<p><a href="product_list.php">Continue Shopping</a></p>
+		</div>
+	</div>
+
+</body>
+</html>
---
+++ kim.jinyeong/product_list.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Product List</title>
+
+	<?php include "parts/meta.php"; ?>
+</head>
+<body>
+
+	<?php include "parts/navbar.php"; ?>
+
+	<div class="container">
+			<h2>Product List</h2>
+
+			<!-- ul>li*4>a[href="product_item.php"]>{Product $} -->
+			
+
+			<?php 
+
+			include_once "lib/php/functions.php";
+			include_once "parts/templates.php";
+
+			$result = makeQuery(
+				makeConn(),
+				"
+				SELECT *
+				FROM `products`
+				ORDER BY `date_create` DESC
+				LIMIT 12
+				"
+			);
+
+			echo "<div class='productlist grid gap'>",array_reduce($result,'productListTemplate'),"</div>";
+
+
+			?>
+	</div>
+
+</body>
+</html>

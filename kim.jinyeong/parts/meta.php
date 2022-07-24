4167620cda9cb35d0ed9c66348db62f9acd49279
jinyeongkim1 <107874179+jinyeongkim1@users.noreply.github.com>
2022-07-24 15:02:28 +0900

    Delete kim.jinyeong directory

--- kim.jinyeong/parts/meta.php
+++
@@ -1,7 +0,0 @@
-<meta name="viewport" content="width=device-width">
-
-<base href="http://jinyeongkim.com/aau/wnm608/kim.jinyeong/">
-
-<link rel="stylesheet" href="lib/css/styleguide.css">
-<link rel="stylesheet" href="lib/css/gridsystem.css">
-<link rel="stylesheet" href="css/storetheme.css">

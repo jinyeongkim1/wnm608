3bce94bd4c307c428f8bc4fb155b17463944e710
jinyeongkim1 <107874179+jinyeongkim1@users.noreply.github.com>
2022-07-31 02:27:52 -0500

    Add files via upload

---
+++ kim.jinyeong/lib/php/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+function MYSQLIAuth() {
+	return [
+		"localhost",  // mysql host
+		"jinyeong", // mysql user name
+		"jinyeong12", // mysql user password
+		"jinyeong" // mysql database name
+	];
+}
